06cf147d234d0242187b8b9fe5c3c31372742e0d
HMISTER <107431892+HMISTER03@users.noreply.github.com>
2023-09-23 17:48:42 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ import requests
 #API Key
 #9957~e21BVu6knGUBYR7kLxHgq0o5HfdwtRKUJ4tSrMoEJ8aI2QwRIhdTDXJ2vC0lZ9VS
 
-canvasAPI = requests.get("https://canvas.instructure.com/api/v1/courses?access_token=9957~e21BVu6knGUBYR7kLxHgq0o5HfdwtRKUJ4tSrMoEJ8aI2QwRIhdTDXJ2vC0lZ9VS")
+canvasAPI = requests.get("https://templeu.instructure.com/api/v1/courses?access_token=9957~e21BVu6knGUBYR7kLxHgq0o5HfdwtRKUJ4tSrMoEJ8aI2QwRIhdTDXJ2vC0lZ9VS")
 raw_data = canvasAPI.json()
 
 params = {'enrollment_term_id': raw_data[0]['enrollment_term_id']}
 courses = [d['id'] for d in raw_data if 'enrollment_term_id' in d and d['enrollment_term_id'] == raw_data[0]['enrollment_term_id']]
 
-courseData = [[requests.get(f"https://canvas.instructure.com/api/v1/courses/{ID}/assignments?access_token=9957~e21BVu6knGUBYR7kLxHgq0o5HfdwtRKUJ4tSrMoEJ8aI2QwRIhdTDXJ2vC0lZ9VS").json()] for ID in courses]
+courseData = [[requests.get(f"https://templeu.instructure.com/api/v1/courses/{ID}/assignments?access_token=9957~e21BVu6knGUBYR7kLxHgq0o5HfdwtRKUJ4tSrMoEJ8aI2QwRIhdTDXJ2vC0lZ9VS").json()] for ID in courses]
 print(courseData)
 assignments = []
 for course in courseData:
@@ -18,4 +18,9 @@ for course in courseData:
             a_course.append((assignment['name'], assignment['due_at']))
         assignments.append(a_course)
 print(assignments)
+
+
+weightData = requests.get(f"https://templeu.instructure.com/api/v1/users/self/enrollments?access_token=9957~e21BVu6knGUBYR7kLxHgq0o5HfdwtRKUJ4tSrMoEJ8aI2QwRIhdTDXJ2vC0lZ9VS").json() 
+#gradeData = requests.get(f"https://canvas.instructure.com/api/v1/users/self/courses?access_token=9957~e21BVu6knGUBYR7kLxHgq0o5HfdwtRKUJ4tSrMoEJ8aI2QwRIhdTDXJ2vC0lZ9VS").json()
+print(weightData)
 
