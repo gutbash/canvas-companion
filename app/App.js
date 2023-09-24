c67ce06a7ba9a2ca6fb99b47aeff83572641a35f
Maya Byrd <mckenzie2331@gmail.com>
2023-09-24 02:59:50 -0400

    update App.js

--- app/App.js
+++ app/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
     return(
+        <>
         <LoginButton />
+        <LogoutButton />
+        </>
     );
 }
 
