918c16da8b4a5c14cb4998415c93ba1f146fa10c
Maya Byrd <mckenzie2331@gmail.com>
2023-09-24 03:49:33 -0400

    Update Profile.js

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,4 +1,4 @@
-/*import React from 'react'
+import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import JSONPretty from 'react-json-pretty';
 
@@ -8,9 +8,9 @@ const Profile = () => {
     return (
         <div>
             <JSONPretty data={user} />
-             {J /*SON.stringify(user, null, 2) */
-        /*</div>/*
+             {J /*SON.stringify(user, null, 2) */}
+        </div>
     )
 }
-/*
-export default Profile */
+
+export default Profile
