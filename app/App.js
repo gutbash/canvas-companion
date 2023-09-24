927e075a69793225fc355530616b0934a1e37981
Maya Byrd <mckenzie2331@gmail.com>
2023-09-24 03:04:25 -0400

    update Profile.js

--- app/App.js
+++ app/App.js
@@ -7,6 +7,7 @@ function App() {
         <>
         <LoginButton />
         <LogoutButton />
+        <Profile />
         </>
     );
 }
---
+++ app/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+
+const Profile = () => {
+    const { user } = useAuth0();
+
+    return (
+        <div>
+            {JSON.stringify(user, null, 2)}
+        </div>
+    )
+}
+
+export default Profile
