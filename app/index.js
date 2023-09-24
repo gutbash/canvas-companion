903b635ee4863ae2c8876e6da0fbb54223e65266
ryan vansluytman <ryan.vansluytman2001@gmail.com>
2023-09-24 04:12:48 -0400

    update Auth0 spelling

--- app/index.js
+++ app/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const domain = process.env.REACT_APP_AUTHO0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTHO0_CLIENT_ID
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 ReactDOM.render(
     <Auth0Provider
