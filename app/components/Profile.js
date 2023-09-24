ba725894c422e76dd2eed9d27f03ca9573b9f32f
ryan vansluytman <ryan.vansluytman2001@gmail.com>
2023-09-24 04:03:04 -0400

    update profile case

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import JSONPretty from 'react-json-pretty';
 
-const Profile = () => {
+const profile = () => {
     const { user } = useAuth0();
 
     return (
