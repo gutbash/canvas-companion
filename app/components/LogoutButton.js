13ecd74005853f762ece75cb41db9a8d69adc80f
Maya Byrd <mckenzie2331@gmail.com>
2023-09-24 03:39:30 -0400

    update Profile.js

--- app/components/LogoutButton.js
+++ app/components/LogoutButton.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 
 const LogoutButton = () => {
-    const { logout } = useAuth0();
+    const { logout, isAuthenticated} = useAuth0();
 
     return (
+        isAuthenticated && (
         <button onClick={() => logout()}>
             Log Out
         </button>
+        )
     )
 }
 
--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import JSONPretty from 'react-json-pretty';
 
 const Profile = () => {
     const { user } = useAuth0();
 
     return (
         <div>
-            {JSON.stringify(user, null, 2)}
+            <JSONPretty data={user} />
+             {J /*SON.stringify(user, null, 2) */}
         </div>
     )
 }
